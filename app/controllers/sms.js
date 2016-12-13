06902fabf564062d56a9336958830a6beb6b96d1
Thomas Apodaca <tma@angrymonkey.com>
2016-12-13 15:49:35 -0800

    req.body instead of POST

--- app/controllers/sms.js
+++ app/controllers/sms.js
@@ -27,10 +27,10 @@ module.exports = {
 
     console.log(req.headers['x-twilio-signature'])
     console.log(credentials.authToken)
-    console.log(twilio.validateRequest(credentials.authToken, req.headers['x-twilio-signature'], 'https://request.clientcomm.org/webhook/sms', POST))
+    console.log(twilio.validateRequest(credentials.authToken, req.headers['x-twilio-signature'], 'https://request.clientcomm.org/webhook/sms', req.body))
 
     // validateRequest returns true if the request originated from Twilio
-    if (twilio.validateRequest(credentials.authToken, req.headers['x-twilio-signature'], 'https://request.clientcomm.org/webhook/sms', POST)) {
+    if (twilio.validateRequest(credentials.authToken, req.headers['x-twilio-signature'], 'https://request.clientcomm.org/webhook/sms', req.body)) {
       // Log IBM Sensitivity measures
       SentimentAnalysis.logIBMSentimentAnalysis(req.body);
       
