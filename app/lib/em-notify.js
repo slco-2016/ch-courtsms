200572816b5e97a3e4d6bde3495f8a5c8180a91a
Thomas Apodaca <tma@angrymonkey.com>
2017-03-02 00:00:50 -0800

    improves readability of email content

--- app/lib/em-notify.js
+++ app/lib/em-notify.js
@@ -82,10 +82,14 @@ module.exports = {
         // send a message through mailgun
         const emailPromises = usersThatNeedToBeAlerted.map((msg, i) => {
           // the message copy that will be emailed
-          const text = ` Hello, ${msg.first} ${msg.last}, this is Code for America. ` +
-                      ` You are receiving this automated email because you have ${msg.count} message(s) waiting for you in ClientComm. ` +
-                      ' To view this message go to ClientComm.org and login with your user name and password. ' +
-                      ' If you are having issues accessing your account, send me an email at clientcomm@codeforamerica.org and we will be happy to assist you any time, day or night!';
+          const text = `Hello, ${msg.first} ${msg.last},` +
+                       `You are receiving this automated email because ` +
+                       `you have ${msg.count} message(s) waiting for you ` +
+                       `in ClientComm. To view this message go to ` +
+                       `ClientComm.org and login with your user name ` +
+                       `and password. If you are having issues accessing ` +
+                       `your account, send an email to clientcomm@codeforamerica.org ` +
+                       `and we will be happy to assist you any time, day or night!`;
 
           // Send mail with defined transport object
           if (credentials.CCENV === 'testing') {
@@ -125,21 +129,23 @@ module.exports = {
   // },
 
   notifyUserFailedSend(cm, msg) {
-    const text = `  Hi, ${cm.first}. You are recieving this email because a message you wrote failed to send. ` +
-                ' This was not your fault - it was likely an error with the SMS service provider. ' +
+    const text = `Hi, ${cm.first}. You are receiving this email because a ` +
+                 `message you wrote failed to send. This was not your fault ` +
+                 `- it was likely an error with the SMS service provider.` +
 
-                ' \n <b> What was the message? </b> ' +
-                ' The contents of the message are included below: \n ' +
-                ` "${msg.content}"` +
+                 `\n<b>What was the message?</b> ` +
+                 `The contents of the message are included below:` +
 
-                ' \n <b> More details: </b> ' +
-                ` The message was sent at ${msg.created}. ` +
-                ` It's last known status was: ${msg.tw_status
+                 `\n"${msg.content}"` +
 
-                } \n <b> What should I do? </b> ` +
-                ' Please check the message and send it again, if needed. ' +
-                ` \n Thanks much and apologies for the inconvenience, ${cm.email
-                } Kuan and Code for America, Team Salt Lake County`;
+                 `\n<b>More details:</b> The message was sent ` +
+                 `at ${msg.created}. ` +
+                 `Its last known status was: ${msg.tw_status}.` +
+
+                 `\n<b>What should I do?</b> ` +
+                 `Please check the message and send it again, if needed.` +
+
+                 `\nThanks much and apologies for the inconvenience.`;
 
     const html = `<p>${text.split('\n').join('</p><p>')}</p>`;
 
@@ -152,11 +158,18 @@ module.exports = {
   },
 
   sendPassResetEmail(cm, uid) {
-    const text = `  Hello, ${cm.first} ${cm.last}. You are recieving this email because your account (${cm.email}) has requested a password reset. ` +
-                ' \n If this was not you, you don\'t need to do anything. If it was you and you did intend to reset your password, please go to the ' +
-                ' following address by either clicking on or cutting and pasting the following address: ' +
-                ` \n ${credentials.baseUrl}/login/reset/${String(uid)
-                } \n The above link will expire within 24 hours. After that time, please request a new key to update your password. `;
+    const text = `Hello, ${cm.first} ${cm.last}. You are receiving this email ` +
+                 `because your account (${cm.email}) has requested a password reset.` +
+
+                 `\nIf this was not you, you don't need to do anything. If it ` +
+                 `was you and you did intend to reset your password, please go to the ` +
+                 `following address by either clicking on or cutting and ` +
+                 `pasting the following address:` +
+
+                 `\n${credentials.baseUrl}/login/reset/${String(uid)}` +
+
+                 `\nThe above link will expire within 24 hours. After that ` +
+                 `time, please request a new key to update your password.`;
 
     const html = `<p>${text.split('\n').join('</p><p>')}</p>`;
 
@@ -165,7 +178,7 @@ module.exports = {
       '"ClientComm - CJS" <clientcomm@codeforamerica.org>',
       'CientComm Password Reset Email',
       html
-    )
+    );
   },
 };
 
