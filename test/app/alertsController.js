f945bb6ec03b933f232f0cd0393f38c67ab8ce36
Thomas Apodaca <tma@angrymonkey.com>
2017-05-03 17:01:00 -0700

    testing unread messages summary
    It's inadequate because we're not testing the actual alert, which is
    generated client-side. A good next step would be to pull as much of the
    creation of the client-side alert to the server side so it can be tested
    with this framework.

--- test/app/alertsController.js
+++ test/app/alertsController.js
@@ -3,25 +3,62 @@ const supertest = require('supertest');
 const should = require('should');
 
 const APP = require('../../app/app');
+const testClient = supertest.agent(APP);
 
-const Departments = require('../../app/models/departments');
-const Users = require('../../app/models/users');
-
-const owner = supertest.agent(APP);
-
-const seededDeptName = 'Pretrial LKJKLJUnique';
-
-describe('Alerts View', () => {
-  // login as the owner account
-  before((done) => {
-    owner.post('/login')
+describe('Alerts controller', () => {
+  // login as the primary account
+  before(done => {
+    testClient
+      .post('/login')
       .type('form')
-      .send({ email: 'owner@test.com' })
+      .send({ email: 'primary@test.com' })
       .send({ pass: '123' })
       .expect(302)
-      .expect('Location', '/')
+      .expect('Location', '/login-success')
       .end((err, res) => {
         done(err);
       });
+  });
+
+  it('returns an accurate summary of unread messages', done => {
+    testClient.get('/alerts').expect(200).end((err, res) => {
+      should.not.exist(err);
+      summary = res.body.newMessageSummary;
+      should.exist(summary);
+
+      // from the seed data, we should have two unread messages from
+      // two active users, and one unread message from an archived user
+      summary.active.messageCount.should.be.exactly(2);
+      summary.active.userCount.should.be.exactly(2);
+      summary.active.userIds.should.containDeep([3, 2]);
+      summary.inactive.messageCount.should.be.exactly(1);
+      summary.inactive.userCount.should.be.exactly(1);
+      summary.inactive.userIds.should.containEql(4);
+      summary.totalUnread.should.be.exactly(3);
+
+      done(err);
+    });
+  });
+
+  it('updates unread messages summary when messages are read', done => {
+    // visit the message stream of one of the clients with unread messages
+    testClient.get('/clients/2/messages/').expect(200).end((err, res) => {
+      testClient.get('/alerts').expect(200).end((err, res) => {
+        summary = res.body.newMessageSummary;
+        should.exist(summary);
+
+        // after this view, we should have one unread message from
+        // one active user, and one unread message from an archived user
+        summary.active.messageCount.should.be.exactly(1);
+        summary.active.userCount.should.be.exactly(1);
+        summary.active.userIds.should.containEql(3);
+        summary.inactive.messageCount.should.be.exactly(1);
+        summary.inactive.userCount.should.be.exactly(1);
+        summary.inactive.userIds.should.containEql(4);
+        summary.totalUnread.should.be.exactly(2);
+
+        done(err);
+      });
+    });
   });
 });
