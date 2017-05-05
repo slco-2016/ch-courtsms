76ea97cb1414d111ff9b0e66ac847f819f954c18
Thomas Apodaca <tma@angrymonkey.com>
2017-05-04 17:25:48 -0700

    Add test for Messages.sendMessage via sms
    There was previously a test for email, but not for SMS. It is somewhat
    difficult to stub the twilio module require, so this commit creates a
    module in app/lib to be stubbed in tests.
    
    Over time, we can opt-in other twilio-based methods into this module as
    we add tests for them.

--- test/app/messagesModel.js
+++ test/app/messagesModel.js
@@ -2,15 +2,18 @@ const assert = require('assert');
 
 require('colors');
 const should = require('should');
-const resourceRequire = require('../../app/lib/resourceRequire');
+const simple = require('simple-mock');
 
+const twClient = require('../../app/lib/twClient');
+
+const resourceRequire = require('../../app/lib/resourceRequire');
 const Messages = resourceRequire('models', 'Messages');
 const Communications = resourceRequire('models', 'Communications');
 const Conversations = resourceRequire('models', 'Conversations');
 
 const mock = resourceRequire('lib', 'mock');
 
-describe('Messages checks', () => {
+describe('Messages model', () => {
   // it('Should be able to create client', function(done) {
   //   Messages.findByUserAndClient()
   //   .then((client) => {
@@ -20,18 +23,51 @@ describe('Messages checks', () => {
   //   }).catch(done);
   // });
 
-  it('should be able to Messages.sendOne email', (done) => {
-  	mock.enable();
-  	let communication,
-    conversation;
-  	Communications.findOneByAttribute('type', 'email')
-  	.then((resp) => {
-  		communication = resp;
-  		return Conversations.findById(1);
-  	}).then(conversation => Messages.sendOne(communication.commid, 'hi', conversation)).then(messages => Messages.where({ content: 'hi', tw_sid: '<2013FAKE82626.18666.16540@clientcomm.org>' })).then((messages) => {
-  		should.exist(messages[0]);
-  		mock.disable();
-  		done();
-  	}).catch(done);
+  it('sends a Messages.sendOne email', done => {
+    mock.enable();
+    let communication, conversation;
+    Communications.findOneByAttribute('type', 'email')
+      .then(resp => {
+        communication = resp;
+        return Conversations.findById(1);
+      })
+      .then(conversation =>
+        Messages.sendOne(communication.commid, 'hi', conversation)
+      )
+      .then(messages =>
+        Messages.where({
+          content: 'hi',
+          tw_sid: '<2013FAKE82626.18666.16540@clientcomm.org>',
+        })
+      )
+      .then(messages => {
+        should.exist(messages[0]);
+        mock.disable();
+        done();
+      })
+      .catch(done);
+  });
+
+  it('sends a sms message', done => {
+    simple.mock(twClient, 'sendMessage')
+      .callbackWith(null, { sid: 123, status: 'Success!' });
+
+    Promise.all([
+      Communications.findById(3),
+      Conversations.findById(3),
+    ]).then(([comm, conversation]) => {
+      const body = 'This is a test message.';
+
+      Messages.sendOne(comm.commid, body, conversation)
+        .then(messages => {
+          twClient.sendMessage.calls.length.should.be.exactly(1);
+          twClient.sendMessage.lastCall.arg.body.should.equal(body);
+          twClient.sendMessage.lastCall.arg.to.should.equal(comm.value);
+
+          simple.restore();
+          done();
+        })
+        .catch(err => done(err));
+    });
   });
 });
