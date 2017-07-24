6716a72cf91a0ad698656bce52b46c2b877c8979
Tomas Apodaca <tma@angrymonkey.com>
2017-07-24 12:19:17 -0700

    pass token on login in test

--- test/app/captureController.js
+++ test/app/captureController.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const supertest = require('supertest');
 const should = require('should');
+const cheerio = require('cheerio');
 
 const APP = require('../../app/app');
 const Communications = require('../../app/models/communications');
@@ -18,49 +19,59 @@ const inboundBodyMsg = createUID();
 
 describe('Capture Board view', () => {
   before((done) => {
-    owner.post('/login')
-      .type('form')
-      .send({ email: 'owner@test.com' })
-      .send({ pass: '123' })
-      .expect(302)
-      .expect('Location', '/login-success')
-      .end((err, res) => {
-        if (err) {
-          done(err);
-        } else {
-          // create a capture board item
-          // this is coming from an unknown number
-          // will just dump onto board as a result
-          const uniqueSMSid = createUID();
-          twilioAgent.post('/webhook/sms')
-            .send({
-              ToCountry: 'US',
-              ToState: 'CA',
-              SmsMessageSid: uniqueSMSid,
-              NumMedia: '0',
-              ToCity: 'SAN DIEGO',
-              FromZip: '92131',
-              SmsSid: uniqueSMSid,
-              FromState: 'CA',
-              SmsStatus: 'received',
-              FromCity: 'SAN DIEGO',
-              Body: inboundBodyMsg,
-              FromCountry: 'US',
-              To: '12435678910',
-              ToZip: '92123',
-              NumSegments: '1',
-              MessageSid: uniqueSMSid,
-              AccountSid: createUID(),
-              From: '%2B10945630939',
-              ApiVersion: '2010-04-01',
-            })
-            .set('X-Twilio-Signature', 'Hwg7BlBJGBLRPcRAlKwKlwtQ+q0=')
-            .expect(200)
-            .end((err, res) => {
+    owner.get('/login').end(function(err, res) {
+      if (res.status == '302') {
+        done();
+      } else {
+        const $html = cheerio.load(res.text);
+        const csrf = $html('input[name=_csrf]').val();
+
+        owner.post('/login')
+          .type('form')
+          .send({ _csrf: csrf })
+          .send({ email: 'owner@test.com' })
+          .send({ pass: '123' })
+          .expect(302)
+          .expect('Location', '/login-success')
+          .end((err, res) => {
+            if (err) {
               done(err);
-            });
-        }
-      });
+            } else {
+              // create a capture board item
+              // this is coming from an unknown number
+              // will just dump onto board as a result
+              const uniqueSMSid = createUID();
+              twilioAgent.post('/webhook/sms')
+                .send({
+                  ToCountry: 'US',
+                  ToState: 'CA',
+                  SmsMessageSid: uniqueSMSid,
+                  NumMedia: '0',
+                  ToCity: 'SAN DIEGO',
+                  FromZip: '92131',
+                  SmsSid: uniqueSMSid,
+                  FromState: 'CA',
+                  SmsStatus: 'received',
+                  FromCity: 'SAN DIEGO',
+                  Body: inboundBodyMsg,
+                  FromCountry: 'US',
+                  To: '12435678910',
+                  ToZip: '92123',
+                  NumSegments: '1',
+                  MessageSid: uniqueSMSid,
+                  AccountSid: createUID(),
+                  From: '%2B10945630939',
+                  ApiVersion: '2010-04-01',
+                })
+                .set('X-Twilio-Signature', 'Hwg7BlBJGBLRPcRAlKwKlwtQ+q0=')
+                .expect(200)
+                .end((err, res) => {
+                  done(err);
+                });
+            }
+          });
+      }
+    });
   });
 
   it('Owner should have permissions to actually view the capture board', (done) => {
