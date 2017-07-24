345044f00102df2da0c4257fa5383018eb907cd2
Tomas Apodaca <tma@angrymonkey.com>
2017-07-24 13:27:00 -0700

    fix communications controller tests

--- test/app/communicationsController.js
+++ test/app/communicationsController.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const supertest = require('supertest');
 const should = require('should');
+const cheerio = require('cheerio');
 
 const APP = require('../../app/app');
 
@@ -15,89 +16,120 @@ const uniquePhoneNumberName = 'unique phone number 1';
 
 describe('Voice requirements', () => {
   before((done) => {
-    primary.post('/login')
-      .send({ email: 'primary@test.com' })
-      .send({ pass: '123' })
-      .expect(302)
-      .expect('Location', '/login-success')
-      .then(() => {
-        // should be able to create a phone number cell type communication
-        primary.post('/clients/2/communications/create')
-          .send({
-            description: uniquePhoneNumberName,
-            type: 'cell',
-            value: uniquePhoneNumber,
-          })
+    primary.get('/login').end(function(err, res) {
+      if (res.status == '302') {
+        done();
+      } else {
+        const $html = cheerio.load(res.text);
+        const csrf_login = $html('input[name=_csrf]').val();
+
+        primary.post('/login')
+          .send({ _csrf: csrf_login })
+          .send({ email: 'primary@test.com' })
+          .send({ pass: '123' })
           .expect(302)
+          .expect('Location', '/login-success')
           .then(() => {
-            Communications.findOneByAttribute('value', uniquePhoneNumber)
-            .then((communication) => {
-              should.equal(communication.type, 'cell');
-              should.equal(communication.description, uniquePhoneNumberName);
-              done();
-            }).catch(done);
-          });
-      });
+            // should be able to create a phone number cell type communication
+            primary.get('/clients/2/communications/create').end(function(err, res) {
+              const $html = cheerio.load(res.text);
+              const csrf_create = $html('input[name=_csrf]').val();
+
+              primary.post('/clients/2/communications/create')
+                .send({ _csrf: csrf_create })
+                .send({
+                  description: uniquePhoneNumberName,
+                  type: 'cell',
+                  value: uniquePhoneNumber,
+                })
+                .expect(302)
+                .then(() => {
+                  Communications.findOneByAttribute('value', uniquePhoneNumber)
+                  .then((communication) => {
+                    should.equal(communication.type, 'cell');
+                    should.equal(communication.description, uniquePhoneNumberName);
+                    done();
+                  }).catch(done);
+                }); // end post create
+            }); // end get create
+          }); // end post login
+      }
+    }); // end get login
   });
 
   it('should be able to create email type communication', (done) => {
-    primary.post('/clients/2/communications/create')
-      .send({
-        description: 'email comm',
-        type: 'email',
-        value: 'test@test.com',
-      })
-      .expect(302)
-      .end((err, res) => {
-        Communications.findOneByAttribute('value', 'test@test.com')
-        .then((communication) => {
-          should.equal(communication.type, 'email');
-          should.equal(communication.description, 'email comm');
-          done();
-        }).catch(done);
-      });
-  });
+    primary.get('/clients/2/communications/create').end(function(err, res) {
+      const $html = cheerio.load(res.text);
+      const csrf = $html('input[name=_csrf]').val();
+      primary.post('/clients/2/communications/create')
+        .send({ _csrf: csrf })
+        .send({
+          description: 'email comm',
+          type: 'email',
+          value: 'test@test.com',
+        })
+        .expect(302)
+        .end((err, res) => {
+          Communications.findOneByAttribute('value', 'test@test.com')
+          .then((communication) => {
+            should.equal(communication.type, 'email');
+            should.equal(communication.description, 'email comm');
+            done();
+          }).catch(done);
+        }); // end post create
+      }); // end get create
+    });
 
   it('should be able to create a phone number cell type communication', (done) => {
-    primary.post('/clients/2/communications/create')
-      .send({
-        description: 'random number',
-        type: 'cell',
-        value: '18288384838',
-      })
-      .expect(302)
-      .end((err, res) => {
-        Communications.findOneByAttribute('value', '18288384838')
-        .then((communication) => {
-          should.equal(communication.type, 'cell');
-          should.equal(communication.description, 'random number');
-          done();
-        }).catch(done);
-      });
+    primary.get('/clients/2/communications/create').end(function(err, res) {
+      const $html = cheerio.load(res.text);
+      const csrf = $html('input[name=_csrf]').val();
+      primary.post('/clients/2/communications/create')
+        .send({ _csrf: csrf })
+        .send({
+          description: 'random number',
+          type: 'cell',
+          value: '18288384838',
+        })
+        .expect(302)
+        .end((err, res) => {
+          Communications.findOneByAttribute('value', '18288384838')
+          .then((communication) => {
+            should.equal(communication.type, 'cell');
+            should.equal(communication.description, 'random number');
+            done();
+          }).catch(done);
+        }); // end post create
+    }); // end get create
   });
 
   it('modifying an existing current phone number', (done) => {
     const newNameForContact = 'unique phone number 1 modified';
-    primary.post('/clients/2/communications/create')
-      .send({
-        description: newNameForContact,
-        type: 'cell',
-        value: uniquePhoneNumber,
-      })
-      .expect(302)
-      .end((err, res) => {
-        Communications.findOneByAttribute('value', uniquePhoneNumber)
-        .then((communication) => {
-          should.equal(communication.type, 'cell');
-          should.equal(communication.description, uniquePhoneNumberName);
+    primary.get('/clients/2/communications/create').end(function(err, res) {
+      const $html = cheerio.load(res.text);
+      const csrf = $html('input[name=_csrf]').val();
+      primary.post('/clients/2/communications/create')
+        .send({ _csrf: csrf })
+        .send({
+          description: newNameForContact,
+          type: 'cell',
+          value: uniquePhoneNumber,
+        })
+        .expect(302)
+        .end((err, res) => {
+          Communications.findOneByAttribute('value', uniquePhoneNumber)
+          .then((communication) => {
+            should.equal(communication.type, 'cell');
+            should.equal(communication.description, uniquePhoneNumberName);
 
-          return CommConns.findManyByAttribute('comm', communication.commid);
-        }).then((commconns) => {
-          // it should have replaced and updated previous commconn
-          should.equal(commconns.length, 1);
-          should.equal(commconns[0].name, newNameForContact);
-          done();
-        }).catch(done);
-      });
+            return CommConns.findManyByAttribute('comm', communication.commid);
+          }).then((commconns) => {
+            // it should have replaced and updated previous commconn
+            should.equal(commconns.length, 1);
+            should.equal(commconns[0].name, newNameForContact);
+            done();
+          }).catch(done);
+        }); // end post create
+    });
   });
 });
